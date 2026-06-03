fd0dccdfd22b7ed60e625451dc35029ea5d6f9c5
Jos Vernon <jv@websupergoo.com>
2026-06-03 10:43:42 +0800

    Updated for ABCpdf .NET Version 14.

--- Form1.cs
+++ Form1.cs
@@ -1,5 +1,5 @@
 ﻿// ===========================================================================
-//	©2013-2024 WebSupergoo. All rights reserved.
+//	©2013-2026 WebSupergoo. All rights reserved.
 //
 //	This source code is for use exclusively with the ABCpdf product with
 //	which it is distributed, under the terms of the license for that
@@ -23,10 +23,10 @@ using System.Text;
 using System.Diagnostics;
 using System.Reflection;
 
-using WebSupergoo.ABCpdf13;
-using WebSupergoo.ABCpdf13.Objects;
-using WebSupergoo.ABCpdf13.Atoms;
-using WebSupergoo.ABCpdf13.Operations;
+using WebSupergoo.ABCpdf14;
+using WebSupergoo.ABCpdf14.Objects;
+using WebSupergoo.ABCpdf14.Atoms;
+using WebSupergoo.ABCpdf14.Operations;
 
 namespace Redaction
 {
--- Redaction.cs
+++ Redaction.cs
@@ -1,5 +1,5 @@
 ﻿// ===========================================================================
-//	©2013-2024 WebSupergoo. All rights reserved.
+//	©2013-2026 WebSupergoo. All rights reserved.
 //
 //	This source code is for use exclusively with the ABCpdf product with
 //	which it is distributed, under the terms of the license for that
@@ -22,10 +22,10 @@ using System.IO;
 using System.Text;
 using System.Diagnostics;
 
-using WebSupergoo.ABCpdf13;
-using WebSupergoo.ABCpdf13.Objects;
-using WebSupergoo.ABCpdf13.Atoms;
-using WebSupergoo.ABCpdf13.Operations;
+using WebSupergoo.ABCpdf14;
+using WebSupergoo.ABCpdf14.Objects;
+using WebSupergoo.ABCpdf14.Atoms;
+using WebSupergoo.ABCpdf14.Operations;
 
 
 namespace Redaction {
